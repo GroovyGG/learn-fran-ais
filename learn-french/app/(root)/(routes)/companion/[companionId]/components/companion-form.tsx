446b257f1932b20a0d092bfc2507cb7277dad1df
Norisama <gloriazbh@gmail.com>
2023-09-11 16:59:14 -0400

    toast, hook, axios in companion-create-form

--- learn-french/app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ learn-french/app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -1,11 +1,10 @@
- "use client";
+"use client";
 
- import * as z from "zod";
-
+import axios from "axios";
+import * as z from "zod";
 import { Category, Companion } from "@prisma/client";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-
 import { 
     Form, 
     FormControl, 
@@ -27,6 +26,8 @@ import {
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Wand2 } from "lucide-react";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
 
 const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.
 `;
@@ -75,6 +76,8 @@ Elon: Always! But right now, I'm particularly excited about Neuralink. It has th
     categories,
     initialData
  }: CompanionFormProps) => {
+    const router = useRouter();
+    const { toast } = useToast();
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: initialData || {
@@ -91,7 +94,26 @@ Elon: Always! But right now, I'm particularly excited about Neuralink. It has th
     const isLoading = form.formState.isSubmitting;
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
+        try {
+            if (initialData) {
+                // update companion functionality
+                await axios.patch(`/api/companion/${initialData.id}`, values);
+            } else {
+                // create companion functionality
+                await axios.post("/api/companion", values);
+            }
+            toast({
+                description: "Your companion has been successfully created.",
+            })
+
+            router.refresh();
+            router.push("/");
+        } catch (error) {
+            toast({
+                variant:"destructive",
+                description: "Something went wrong. Please try again."
+            });
+        }
     }
 
     return (
