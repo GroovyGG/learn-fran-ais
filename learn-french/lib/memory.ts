5f3ec514c533ef071dc4c2a6293e33ea21f66f03
Norisama <gloriazbh@gmail.com>
2023-09-12 15:15:52 -0400

    add memory and rate limit

--- learn-french/lib/memory.ts
+++ learn-french/lib/memory.ts
@@ -2,23 +2,44 @@ import { Redis } from "@upstash/redis";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { PineconeClient } from "@pinecone-database/pinecone";
 import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { SupabaseVectorStore } from "langchain/vectorstores/supabase";
+import { SupabaseClient, createClient } from "@supabase/supabase-js";
 
+
+// A CompanionKey type is defined to represent a unique identifier for the chat companion. It combines the companion's name, the model's name, and the user's ID.
 export type CompanionKey = {
   companionName: string;
   modelName: string;
   userId: string;
 };
 
-export class MemoryManager {
+
+
+// Private Properties:
+// history: An instance of Redis for storing the chat history.
+// vectorDBClient: Can either be an instance of PineconeClient or SupabaseClient, depending on the environment configuration.
+class MemoryManager {
   private static instance: MemoryManager;
   private history: Redis;
-  private vectorDBClient: PineconeClient;
+  private vectorDBClient: PineconeClient | SupabaseClient;
 
   public constructor() {
     this.history = Redis.fromEnv();
-    this.vectorDBClient = new PineconeClient();
+    if (process.env.VECTOR_DB === "pinecone") {
+      this.vectorDBClient = new PineconeClient();
+    } else {
+      const auth = {
+        detectSessionInUrl: false,
+        persistSession: false,
+        autoRefreshToken: false,
+      };
+      const url = process.env.SUPABASE_URL!;
+      const privateKey = process.env.SUPABASE_PRIVATE_KEY!;
+      this.vectorDBClient = createClient(url, privateKey, { auth });
+    }
   }
 
+  // Initializes the vectorDBClient if it's an instance of PineconeClient.
   public async init() {
     if (this.vectorDBClient instanceof PineconeClient) {
       await this.vectorDBClient.init({
@@ -28,29 +49,53 @@ export class MemoryManager {
     }
   }
 
+  //  vectorSearch method:
+  //  Performs a vector similarity search on the provided chat history using either Pinecone or Supabase, depending on the environment configuration.
+  //  The vector search returns a list of documents that are similar to the provided chat history.
   public async vectorSearch(
     recentChatHistory: string,
     companionFileName: string
   ) {
-    const pineconeClient = <PineconeClient>this.vectorDBClient;
+    if (process.env.VECTOR_DB === "pinecone") {
+      console.log("INFO: using Pinecone for vector search.");
+      const pineconeClient = <PineconeClient>this.vectorDBClient;
 
-    const pineconeIndex = pineconeClient.Index(
-      process.env.PINECONE_INDEX! || ""
-    );
+      const pineconeIndex = pineconeClient.Index(
+        process.env.PINECONE_INDEX! || ""
+      );
 
-    const vectorStore = await PineconeStore.fromExistingIndex(
-      new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY }),
-      { pineconeIndex }
-    ); 
+      const vectorStore = await PineconeStore.fromExistingIndex(
+        new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY }),
+        { pineconeIndex }
+      );
 
-    const similarDocs = await vectorStore
-      .similaritySearch(recentChatHistory, 3, { fileName: companionFileName })
-      .catch((err) => {
-        console.log("WARNING: failed to get vector search results.", err);
-      });
-    return similarDocs;
+      const similarDocs = await vectorStore
+        .similaritySearch(recentChatHistory, 3, { fileName: companionFileName })
+        .catch((err) => {
+          console.log("WARNING: failed to get vector search results.", err);
+        });
+      return similarDocs;
+    } else {
+      console.log("INFO: using Supabase for vector search.");
+      const supabaseClient = <SupabaseClient>this.vectorDBClient;
+      const vectorStore = await SupabaseVectorStore.fromExistingIndex(
+        new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY }),
+        {
+          client: supabaseClient,
+          tableName: "documents",
+          queryName: "match_documents",
+        }
+      );
+      const similarDocs = await vectorStore
+        .similaritySearch(recentChatHistory, 3)
+        .catch((err) => {
+          console.log("WARNING: failed to get vector search results.", err);
+        });
+      return similarDocs;
+    }
   }
 
+  // This is a static method that returns a singleton instance of the MemoryManager class. If an instance does not already exist, it creates one and initializes it.
   public static async getInstance(): Promise<MemoryManager> {
     if (!MemoryManager.instance) {
       MemoryManager.instance = new MemoryManager();
@@ -58,11 +103,12 @@ export class MemoryManager {
     }
     return MemoryManager.instance;
   }
-
+  // Generates a unique Redis key by combining values from the CompanionKey.
   private generateRedisCompanionKey(companionKey: CompanionKey): string {
     return `${companionKey.companionName}-${companionKey.modelName}-${companionKey.userId}`;
   }
 
+  // Adds the provided text to the Redis chat history associated with the given companionKey.
   public async writeToHistory(text: string, companionKey: CompanionKey) {
     if (!companionKey || typeof companionKey.userId == "undefined") {
       console.log("Companion key set incorrectly");
@@ -78,6 +124,7 @@ export class MemoryManager {
     return result;
   }
 
+  // Retrieves the most recent chat history for the given companionKey from Redis.
   public async readLatestHistory(companionKey: CompanionKey): Promise<string> {
     if (!companionKey || typeof companionKey.userId == "undefined") {
       console.log("Companion key set incorrectly");
@@ -94,6 +141,7 @@ export class MemoryManager {
     return recentChats;
   }
 
+  // Prepopulates the chat history for a new user using a seed content.
   public async seedChatHistory(
     seedContent: String,
     delimiter: string = "\n",
@@ -113,3 +161,5 @@ export class MemoryManager {
     }
   }
 }
+
+export default MemoryManager;
--- learn-french/lib/rate-limit.ts
+++ learn-french/lib/rate-limit.ts
@@ -11,3 +11,21 @@ export async function rateLimit(identifier: string) {
 
   return await ratelimit.limit(identifier);
 };
+
+
+
+// rateLimit Function
+// This function is exported and is designed to apply a rate limiting check:
+
+// Parameters:
+// identifier: string: An identifier that's likely used to uniquely identify a user, IP address, or any entity you want to rate limit.
+// Inside the Function:
+// A new Ratelimit instance is created with the following configuration:
+
+// redis: It sets up a connection to a Redis database using environment variables (probably from a .env file or system environment variables).
+// limiter: This specifies the rate limiting strategy. Here, a sliding window strategy is used, which allows up to 10 requests within a 10-second window.
+// analytics: It is set to true, which probably means that the rate limiter will keep analytics or metrics about the rate limiting events.
+// prefix: A string prefix that might be used for storing rate limit data in Redis.
+// The function then calls the limit method on the ratelimit instance, passing the identifier to it. This method will determine if the given identifier has exceeded its rate limits.
+
+// The result of the limit method is awaited (since it's likely asynchronous) and returned. This result will probably indicate whether the request associated with the given identifier should be allowed or if it's been rate-limited.
