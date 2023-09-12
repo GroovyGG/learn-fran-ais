bcb55d916509f8250f9b59ad9a553c3e3bb46c64
Norisama <gloriazbh@gmail.com>
2023-09-12 16:15:47 -0400

    update memory to make it work

--- learn-french/lib/memory.ts
+++ learn-french/lib/memory.ts
@@ -2,44 +2,23 @@ import { Redis } from "@upstash/redis";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { PineconeClient } from "@pinecone-database/pinecone";
 import { PineconeStore } from "langchain/vectorstores/pinecone";
-import { SupabaseVectorStore } from "langchain/vectorstores/supabase";
-import { SupabaseClient, createClient } from "@supabase/supabase-js";
 
-
-// A CompanionKey type is defined to represent a unique identifier for the chat companion. It combines the companion's name, the model's name, and the user's ID.
 export type CompanionKey = {
   companionName: string;
   modelName: string;
   userId: string;
 };
 
-
-
-// Private Properties:
-// history: An instance of Redis for storing the chat history.
-// vectorDBClient: Can either be an instance of PineconeClient or SupabaseClient, depending on the environment configuration.
-class MemoryManager {
+export class MemoryManager {
   private static instance: MemoryManager;
   private history: Redis;
-  private vectorDBClient: PineconeClient | SupabaseClient;
+  private vectorDBClient: PineconeClient;
 
   public constructor() {
     this.history = Redis.fromEnv();
-    if (process.env.VECTOR_DB === "pinecone") {
-      this.vectorDBClient = new PineconeClient();
-    } else {
-      const auth = {
-        detectSessionInUrl: false,
-        persistSession: false,
-        autoRefreshToken: false,
-      };
-      const url = process.env.SUPABASE_URL!;
-      const privateKey = process.env.SUPABASE_PRIVATE_KEY!;
-      this.vectorDBClient = createClient(url, privateKey, { auth });
-    }
+    this.vectorDBClient = new PineconeClient();
   }
 
-  // Initializes the vectorDBClient if it's an instance of PineconeClient.
   public async init() {
     if (this.vectorDBClient instanceof PineconeClient) {
       await this.vectorDBClient.init({
@@ -49,66 +28,43 @@ class MemoryManager {
     }
   }
 
-  //  vectorSearch method:
-  //  Performs a vector similarity search on the provided chat history using either Pinecone or Supabase, depending on the environment configuration.
-  //  The vector search returns a list of documents that are similar to the provided chat history.
   public async vectorSearch(
     recentChatHistory: string,
     companionFileName: string
   ) {
-    if (process.env.VECTOR_DB === "pinecone") {
-      console.log("INFO: using Pinecone for vector search.");
-      const pineconeClient = <PineconeClient>this.vectorDBClient;
+    const pineconeClient = <PineconeClient>this.vectorDBClient;
 
-      const pineconeIndex = pineconeClient.Index(
-        process.env.PINECONE_INDEX! || ""
-      );
+    const pineconeIndex = pineconeClient.Index(
+      process.env.PINECONE_INDEX! || ""
+    );
 
-      const vectorStore = await PineconeStore.fromExistingIndex(
-        new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY }),
-        { pineconeIndex }
-      );
+    const vectorStore = await PineconeStore.fromExistingIndex(
+      new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY }),
+      { pineconeIndex }
+    );
 
-      const similarDocs = await vectorStore
-        .similaritySearch(recentChatHistory, 3, { fileName: companionFileName })
-        .catch((err) => {
-          console.log("WARNING: failed to get vector search results.", err);
-        });
-      return similarDocs;
-    } else {
-      console.log("INFO: using Supabase for vector search.");
-      const supabaseClient = <SupabaseClient>this.vectorDBClient;
-      const vectorStore = await SupabaseVectorStore.fromExistingIndex(
-        new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY }),
-        {
-          client: supabaseClient,
-          tableName: "documents",
-          queryName: "match_documents",
-        }
-      );
-      const similarDocs = await vectorStore
-        .similaritySearch(recentChatHistory, 3)
-        .catch((err) => {
-          console.log("WARNING: failed to get vector search results.", err);
-        });
-      return similarDocs;
-    }
+    const similarDocs = await vectorStore
+      .similaritySearch(recentChatHistory, 3, { fileName: companionFileName })
+      .catch((err) => {
+        console.log("Failed to get vector search results", err);
+      });
+
+    return similarDocs;
   }
 
-  // This is a static method that returns a singleton instance of the MemoryManager class. If an instance does not already exist, it creates one and initializes it.
   public static async getInstance(): Promise<MemoryManager> {
     if (!MemoryManager.instance) {
       MemoryManager.instance = new MemoryManager();
       await MemoryManager.instance.init();
     }
+
     return MemoryManager.instance;
   }
-  // Generates a unique Redis key by combining values from the CompanionKey.
+
   private generateRedisCompanionKey(companionKey: CompanionKey): string {
     return `${companionKey.companionName}-${companionKey.modelName}-${companionKey.userId}`;
   }
 
-  // Adds the provided text to the Redis chat history associated with the given companionKey.
   public async writeToHistory(text: string, companionKey: CompanionKey) {
     if (!companionKey || typeof companionKey.userId == "undefined") {
       console.log("Companion key set incorrectly");
@@ -124,7 +80,6 @@ class MemoryManager {
     return result;
   }
 
-  // Retrieves the most recent chat history for the given companionKey from Redis.
   public async readLatestHistory(companionKey: CompanionKey): Promise<string> {
     if (!companionKey || typeof companionKey.userId == "undefined") {
       console.log("Companion key set incorrectly");
@@ -141,13 +96,13 @@ class MemoryManager {
     return recentChats;
   }
 
-  // Prepopulates the chat history for a new user using a seed content.
   public async seedChatHistory(
     seedContent: String,
     delimiter: string = "\n",
     companionKey: CompanionKey
   ) {
     const key = this.generateRedisCompanionKey(companionKey);
+
     if (await this.history.exists(key)) {
       console.log("User already has chat history");
       return;
@@ -155,11 +110,41 @@ class MemoryManager {
 
     const content = seedContent.split(delimiter);
     let counter = 0;
+
     for (const line of content) {
       await this.history.zadd(key, { score: counter, member: line });
       counter += 1;
     }
   }
-}
 
-export default MemoryManager;
+  public async deleteChatHistory(companionKey: CompanionKey) {
+    if (!companionKey || typeof companionKey.userId == "undefined") {
+      console.log("Companion key set incorrectly");
+      return "";
+    }
+
+    const key = this.generateRedisCompanionKey(companionKey);
+
+    await this.history.del(key);
+    console.log("Chat history deleted from redis");
+  }
+
+  public async deleteCompanionHistory(companion: string) {
+    if (!companion || typeof companion == "undefined") {
+      console.log("Companion not detected");
+      return "";
+    }
+
+    const keyList = await this.history.keys(companion + "*");
+
+    console.log(keyList);
+
+    await Promise.all(
+      keyList.map(async (key) => {
+        await this.history.del(key);
+      })
+    );
+
+    console.log("All keys are deleted");
+  }
+}
